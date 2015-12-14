68e4e0a0cc76117fdc2bcf16790b9fe11c8f5a98
Peter Pham <ptr.pham@gmail.com>
2015-12-13 21:27:24 -0800

    Adding missing sounds.js

---
+++ sounds.js
@@ -0,0 +1,39 @@
+
+function loadResource(path, type) {
+  type = type || 'arraybuffer';
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.responseType = type
+    req.open('GET', path, true);
+    req.onload = function() { return resolve(req.response); }
+    req.send();
+  });
+}
+
+function Sound(ctx, path) {
+  var _this = this;
+  this.ctx = ctx;
+  loadResource(path).then(function(data) {
+    ctx.decodeAudioData(data, function(buffer) { _this.buffer = buffer; });
+  });
+}
+
+Sound.prototype.play = function() {
+  if (this.buffer == null || this.playing) return Promise.resolve(true);
+  var ctx = this.ctx;
+  var _this = this;
+
+  var result = ctx.createBufferSource();
+  result.connect(ctx.destination);
+  result.buffer = this.buffer;
+  this.playing = true;
+  result.start();
+
+  return new Promise(function(resolve, reject) {
+    result.onended = function() {
+      _this.playing = false;
+      resolve(true);
+    };
+  });
+};
+
